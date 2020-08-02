bcd10346af60a295aaa6b327feec01e5c4b24a12
Ivan Tolkachev <vantrey@yandex.ru>
2020-08-02 11:27:51 +0300

    body parser for PUT

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const {getUsers, addUser} = require('./repository');
 const express = require('express');
-const users = require('./usersRouter')
+const users = require('./usersRouter');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 
 // create express app
 const app = express();
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // as a base url
-app.use('/users', users)
+app.use('/users', users);
 
 app.get('/tasks', async (req, res) => {
     res.send('tasks');
--- usersRouter.js
+++ usersRouter.js
@@ -10,7 +10,13 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/', async (req, res) => {
-    const users = await getUsers();
+    let users = await getUsers();
+    const search = req.query.search;
+
+    if (search) {
+        users = users.filter(u => u.name.indexOf(search) > -1);
+    }
+
     res.send(users);
 });
 
@@ -18,7 +24,7 @@ router.get('/:id', async (req, res) => {
     const users = await getUsers();
     const userId = req.params.id
     const user = users.find(u => u.id === Number(userId));
-    if(user) {
+    if (user) {
         res.send(user);
     } else {
         res.send(404);
@@ -27,7 +33,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    await addUser('Lesh');
+    const name = req.body.name;
+    await addUser(name);
     res.send({success: true});
 });
 
